f2a0d3517f22aca2e83cb1b1375b7a41a1799135
Connor Love <connor@dotconnor.com>
2019-02-27 12:14:08 -0500

    add initial content

---
+++ readme.md
@@ -0,0 +1,30 @@
+# detect
+
+Browser/OS detections
+
+## Install
+
+```shell
+yarn add @a1motion/detect
+```
+
+## Usage
+
+```javascript
+import detect from "@a1motion/detect";
+
+detect = {
+  ie: boolean;
+  safari: boolean;
+  firefox: boolean;
+  opera: boolean;
+  chrome: boolean;
+  mac: boolean;
+  win: boolean;
+  ios: boolean;
+  android: boolean;
+  linux: boolean;
+  desktop: boolean;
+  mobile: boolean;
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+declare global {
+  interface Document {
+    documentMode?: any;
+  }
+  interface Window {
+    HTMLElement?: any;
+    safari?: any;
+    InstallTrigger?: any;
+    opr?: any;
+    opera?: any;
+    chrome?: any;
+  }
+}
+export default (() => {
+  const { platform, userAgent } = window.navigator;
+  const mac =
+    ["Macintosh", "MacIntel", "MacPPC", "Mac68K"].indexOf(platform) !== -1;
+  const win = ["Win32", "Win64", "Windows", "WinCE"].indexOf(platform) !== -1;
+  const ios = ["iPhone", "iPad", "iPod"].indexOf(platform) !== -1;
+  const android = !mac && !win && !ios && /Android/.test(userAgent);
+  const linux = !mac && !win && !ios && !android && /Linux/.test(platform);
+  return {
+    ie: /*@cc_on!@*/ false || !!document.documentMode,
+    safari:
+      /constructor/i.test(window.HTMLElement) ||
+      (function(p) {
+        return p.toString() === "[object SafariRemoteNotification]";
+      })(
+        !window.safari ||
+          (typeof window.safari !== "undefined" &&
+            window.safari.pushNotification)
+      ),
+    firefox: typeof window.InstallTrigger !== "undefined",
+    opera:
+      (!!window.opr && !!window.opr.addons) ||
+      !!window.opera ||
+      navigator.userAgent.indexOf(" OPR/") >= 0,
+    chrome:
+      !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime),
+    mac,
+    win,
+    ios,
+    android,
+    linux,
+    desktop: mac || win || linux,
+    mobile: ios || android
+  };
+})();
